9a0aa41bba5a343d3ca237b6793806aa3f3c9f14
Maksim Ivanov <omaxphp@yandex.ru>
2023-08-20 23:34:04 +0300

    feat: support prettier 3.x (#2)

--- projects/prettier-config/plugins/prettier-plugin-sort-package.js
+++ projects/prettier-config/plugins/prettier-plugin-sort-package.js
@@ -1,4 +1,5 @@
-const sortPackageJson = require('sort-package-json');
+const prettier = import('prettier');
+const sortPackageJson = import('sort-package-json');
 const {parsers} = require('prettier/parser-babel');
 
 const parser = parsers['json-stringify'];
@@ -6,31 +7,38 @@ const parser = parsers['json-stringify'];
 exports.parsers = {
     'json-stringify': {
         ...parser,
-        preprocess(text, options) {
-            const processed = parser.preprocess ? parser.preprocess(text, options) : text;
-
+        async parse(text, options) {
             const isPackageJson =
                 options.filepath &&
                 /package\.json$|ng-package\.json$/.test(options.filepath);
 
-            if (isPackageJson) {
-                const json = JSON.parse(processed);
-                const unsortedScripts = deepClone((json && json.scripts) || {});
-                const sorted = sortPackageJson(json);
-
-                /**
-                 * @note: add the scripts field if it's provided
-                 * the scripts must be unsorted
-                 */
-                // eslint-disable-next-line no-prototype-builtins
-                if (json && json.hasOwnProperty('scripts')) {
-                    sorted.scripts = unsortedScripts;
-                }
-
-                return JSON.stringify(sorted);
+            if (!isPackageJson) {
+                return parser.parse(text, options);
             }
 
-            return processed;
+            // To avoid parsing errors
+            text = await (await prettier).format(text, {filepath: options.filepath});
+
+            if (parser.preprocess) {
+                text = parser.preprocess(text, options);
+            }
+
+            const json = JSON.parse(text);
+            const unsortedScripts = deepClone((json && json.scripts) || {});
+            const sorted = (await sortPackageJson).default(json);
+
+            /**
+             * @note: add the scripts field if it's provided
+             * the scripts must be unsorted
+             */
+            // eslint-disable-next-line no-prototype-builtins
+            if (json && json.hasOwnProperty('scripts')) {
+                sorted.scripts = unsortedScripts;
+            }
+
+            text = JSON.stringify(sorted);
+
+            return parser.parse(text, options);
         },
     },
 };
