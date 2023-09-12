b341e9d7b02f2ed8515678d40bdb0e1c12d7ecf1
splincode <omaxphp@yandex.ru>
2023-09-12 10:52:11 +0300

    fix(commitlint-config): add new scopes

--- projects/commitlint-config/commitlint.config.js
+++ projects/commitlint-config/commitlint.config.js
@@ -2,17 +2,31 @@ module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
         'scope-enum': () => {
-            try {
-                const {readdirSync, statSync} = require('fs');
-                const dir = 'projects';
-                const projectsNames = readdirSync(dir).filter(entity =>
-                    statSync(`${dir}/${entity}`).isDirectory(),
-                );
-
-                return [2, 'always', projectsNames];
-            } catch {
-                return [0];
+            function getTypes(dir) {
+                try {
+                    const {readdirSync, statSync} = require('fs');
+                    return readdirSync(dir).filter(entity =>
+                        statSync(`${dir}/${entity}`).isDirectory(),
+                    );
+                } catch {
+                    return [];
+                }
             }
+
+            return [
+                2,
+                'always',
+                [
+                    'deprecate',
+                    'schematics',
+                    'all',
+                    'deps',
+                    ...getTypes('projects'),
+                    ...getTypes('apps'),
+                    ...getTypes('libs'),
+                    ...getTypes('packages'),
+                ],
+            ];
         },
         'type-enum': () => {
             const [level, applicable, types] = require('@commitlint/config-conventional')
