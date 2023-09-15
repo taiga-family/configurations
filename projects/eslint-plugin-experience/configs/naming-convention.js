acf28d9fb0f642a08b85cad8463986a560e5df5d
splincode <omaxphp@yandex.ru>
2023-09-15 15:32:19 +0400

    feat(eslint-plugin-experience): remove taiga prefix

--- projects/eslint-plugin-experience/configs/naming-convention.js
+++ projects/eslint-plugin-experience/configs/naming-convention.js
@@ -15,25 +15,16 @@ module.exports = {
                         selector: 'class',
                         modifiers: ['exported'],
                         format: ['PascalCase'],
-                        prefix: ['Tui'],
-                    },
-                    {
-                        selector: 'class',
-                        modifiers: ['exported', 'abstract'],
-                        format: ['PascalCase'],
-                        prefix: ['AbstractTui'],
                     },
                     {
                         selector: 'function',
                         modifiers: ['exported'],
-                        format: ['PascalCase'],
-                        prefix: ['tui'],
+                        format: ['camelCase'],
                     },
                     {
                         selector: 'interface',
                         modifiers: ['exported'],
                         format: ['PascalCase'],
-                        prefix: ['Tui'],
                     },
                     {
                         selector: 'variable',
@@ -58,13 +49,12 @@ module.exports = {
                         selector: 'class',
                         modifiers: ['abstract'],
                         format: ['PascalCase'],
-                        prefix: ['AbstractTui'],
+                        prefix: ['Abstract'],
                     },
                     {
                         selector: 'enum',
                         modifiers: ['exported'],
                         format: ['StrictPascalCase'],
-                        prefix: ['Tui'],
                     },
                     {
                         selector: 'enumMember',
@@ -74,77 +64,9 @@ module.exports = {
                         selector: 'classMethod',
                         format: ['strictCamelCase'],
                         filter: {
-                            regex: '(HTML|JSON|CVC|CVCChange|DOM)$',
+                            regex: '(HTML|CSS|TS|JSON|CVC|CVCChange|URI|URL|DOM)$',
                             match: false,
                         },
-                    },
-                ],
-            },
-        },
-        {
-            files: [
-                '**/projects/*demo/**/*.ts',
-                '**/scripts/**/*.ts',
-                '**/schematics/**/*.ts',
-                '**/apps/**/*.ts',
-            ],
-            parser: '@typescript-eslint/parser',
-            plugins: ['@typescript-eslint'],
-            rules: {
-                '@typescript-eslint/naming-convention': [
-                    'error',
-                    {
-                        selector: 'typeLike',
-                        format: ['PascalCase', 'UPPER_CASE'],
-                    },
-                    {
-                        selector: 'class',
-                        modifiers: ['exported'],
-                        format: ['PascalCase'],
-                    },
-                    {
-                        selector: 'function',
-                        modifiers: ['exported'],
-                        format: ['camelCase'],
-                    },
-                    {
-                        selector: 'interface',
-                        modifiers: ['exported'],
-                        format: ['PascalCase'],
-                    },
-                    {
-                        selector: 'variable',
-                        modifiers: ['destructured'],
-                        format: null,
-                    },
-                    {
-                        selector: 'variable',
-                        format: ['camelCase'],
-                    },
-                    {
-                        selector: 'variable',
-                        modifiers: ['global'],
-                        format: ['UPPER_CASE', 'camelCase', 'PascalCase'],
-                    },
-                    {
-                        selector: 'variable',
-                        modifiers: ['exported'],
-                        format: ['UPPER_CASE', 'camelCase', 'PascalCase'],
-                    },
-                    {
-                        selector: 'class',
-                        modifiers: ['abstract'],
-                        format: ['PascalCase'],
-                        prefix: ['Abstract', 'TuiAbstract', 'Example'],
-                    },
-                    {
-                        selector: 'enum',
-                        modifiers: ['exported'],
-                        format: ['StrictPascalCase'],
-                    },
-                    {
-                        selector: 'enumMember',
-                        format: ['PascalCase'],
                     },
                 ],
             },
