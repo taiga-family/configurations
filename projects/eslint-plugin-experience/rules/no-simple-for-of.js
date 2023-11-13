26214becf271337a80b28921a24073e8b0b0eac4
splincode <omaxphp@yandex.ru>
2023-11-13 20:23:02 +0300

    feat(eslint-plugin-experience): ignore ReturnStatement

--- projects/eslint-plugin-experience/rules/no-simple-for-of.js
+++ projects/eslint-plugin-experience/rules/no-simple-for-of.js
@@ -17,6 +17,7 @@ module.exports = {
                     'AwaitExpression',
                     'BreakStatement',
                     'ContinueStatement',
+                    'ReturnStatement',
                 ]);
 
                 if (isSimpleForOf) {
