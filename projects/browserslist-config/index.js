319fb118f32273489dc97f61f850d512381c340d
splincode <omaxphp@yandex.ru>
2024-02-19 12:49:30 +0300

    feat(browserslist-config): upgrade config

--- projects/browserslist-config/index.js
+++ projects/browserslist-config/index.js
@@ -1,12 +1,12 @@
 module.exports = [
-    // Desktop
-    'Chrome >= 74',
-    'Firefox >= 67',
-    'Safari >= 12.1',
-    'Edge >= 79',
-    'Opera >= 62',
-    // Mobile
-    'iOS >= 12.1',
-    'ChromeAndroid >= 90',
-    'FirefoxAndroid >= 99',
+    'ChromeAndroid >= 88',
+    'Chrome >= 88',
+    'Edge >= 88',
+    'Opera >= 74',
+    'OperaMobile >= 63',
+    'iOS >= 13.4',
+    'Safari >= 13.1',
+    'Samsung >= 15',
+    'Firefox >= 120',
+    'FirefoxAndroid >= 120',
 ];
