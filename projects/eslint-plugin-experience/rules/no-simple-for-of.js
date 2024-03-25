9ac448904f916b54c199979c714ca8f504c2f551
splincode <omaxphp@yandex.ru>
2024-03-25 10:33:30 +0300

    fix(eslint-plugin-experience): exclude yield expression

--- projects/eslint-plugin-experience/rules/no-simple-for-of.js
+++ projects/eslint-plugin-experience/rules/no-simple-for-of.js
@@ -11,6 +11,7 @@ module.exports = {
             ForOfStatement(node) {
                 const isSimpleForOf = !findExpressions(node, [
                     'AwaitExpression',
+                    'YieldExpression',
                     'BreakStatement',
                     'ContinueStatement',
                     'ReturnStatement',
