70c9c323fa76785b98e915bc29b0a7430913b9f6
Nikita Barsukov <nikita.s.barsukov@gmail.com>
2024-06-21 12:45:08 +0300

    feat(eslint-plugin-experience): new config `@taiga-ui/experience/entry-points`

---
+++ projects/eslint-plugin-experience/configs/entry-points.js
@@ -0,0 +1,34 @@
+const {globSync} = require('glob');
+const {readFileSync} = require('fs');
+
+const allPackageJSONs = globSync(`**/package.json`, {
+    ignore: ['node_modules/**', 'dist/**'],
+}).filter(path => !readJSON(path).private);
+const packageNames = allPackageJSONs.map(path => readJSON(path).name);
+
+module.exports = {
+    overrides: [
+        {
+            files: allPackageJSONs.map(path => path.replace('package.json', '**')),
+            parser: '@typescript-eslint/parser',
+            excludedFiles: ['**/*.spec.ts', '**/*.cy.ts'],
+            rules: {
+                '@taiga-ui/experience/no-deep-imports': 'off',
+                '@taiga-ui/experience/prefer-deep-imports': [
+                    'error',
+                    {
+                        importFilter: packageNames,
+                    },
+                ],
+            },
+        },
+    ],
+};
+
+function readJSON(path) {
+    try {
+        return JSON.parse(readFileSync(path, 'utf8'));
+    } catch {
+        return '';
+    }
+}
