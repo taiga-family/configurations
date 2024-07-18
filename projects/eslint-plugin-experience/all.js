b861d5ad4f9b15abddbb4c87932ee4355a35dbf7
splincode <omaxphp@yandex.ru>
2024-07-18 11:41:45 +0300

    feat(eslint-plugin-experience): disable progress on CI

--- projects/eslint-plugin-experience/all.js
+++ projects/eslint-plugin-experience/all.js
@@ -70,7 +70,21 @@ module.exports = {
         },
     },
     rules: {
-        'file-progress/activate': 1,
+        'file-progress/activate':
+            process.env.TUI_CI === 'true' ||
+            !!process.env.CI || // Another CI
+            !!process.env.GITHUB_ACTION || // Github CI
+            !!process.env.GITLAB_CI || // Gitlab CI
+            !!process.env.CIRCLECI || // Circle CI
+            !!process.env.TF_BUILD || // Azure CI
+            !!process.env.CIRRUS_CI || // Cirrus CI
+            !!process.env.BUILDKITE || // Build Kite CI
+            !!process.env.HEROKU_TEST_RUN_ID || // Heroku CI
+            !!process.env.CODEBUILD_BUILD_ID || // CodeBuild CI
+            !!process.env.TEAMCITY_VERSION || // TeamCity CI
+            !!process.env.BUILD_ID // Jenkins/Hudson
+                ? 0
+                : 1,
     },
     overrides: [
         {
