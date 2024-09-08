d80e34a0383fe8fb3aee0e7ac7f00ca99aff056f
splincode <omaxphp@yandex.ru>
2024-09-08 10:53:00 +0300

    feat: update stylelint

--- projects/stylelint-config/index.js
+++ projects/stylelint-config/index.js
@@ -18,68 +18,30 @@ module.exports = {
                 exceptProperties: ['opacity'],
             },
         ],
-        'annotation-no-unknown': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
+        'annotation-no-unknown': true,
+        'at-rule-allowed-list': ['extend', 'keyframes', 'import', 'media', 'supports'],
         'at-rule-empty-line-before': [
             'always',
             {
                 except: ['first-nested'],
                 ignore: ['after-comment', 'blockless-after-same-name-blockless'],
-                severity: 'error',
             },
         ],
-        'at-rule-no-unknown': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
-        'at-rule-no-vendor-prefix': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
-        'block-no-empty': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
+        'at-rule-no-unknown': true,
+        'at-rule-no-vendor-prefix': true,
+        'block-no-empty': true,
         'color-function-notation': [
             'modern',
             {
                 ignore: ['with-var-inside'],
             },
         ],
-        'color-hex-length': [
-            'short',
-            {
-                severity: 'error',
-            },
-        ],
-        'color-no-invalid-hex': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
-        'comment-no-empty': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
-        'comment-whitespace-inside': [
-            'always',
-            {
-                severity: 'error',
-            },
-        ],
+        'color-hex-alpha': 'never',
+        'color-hex-length': 'short',
+        'color-named': 'never',
+        'color-no-invalid-hex': true,
+        'comment-no-empty': true,
+        'comment-whitespace-inside': 'always',
         'csstools/use-logical': [
             'always',
             {
@@ -100,138 +62,69 @@ module.exports = {
             {
                 except: ['after-custom-property', 'first-nested'],
                 ignore: ['after-comment'],
-                severity: 'error',
             },
         ],
-        'custom-property-no-missing-var-function': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
-        'declaration-block-no-duplicate-custom-properties': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
+        'custom-property-no-missing-var-function': true,
+        'declaration-block-no-duplicate-custom-properties': true,
         'declaration-block-no-duplicate-properties': [
             true,
             {
                 ignore: ['consecutive-duplicates'],
-                severity: 'error',
             },
         ],
-        'declaration-block-no-redundant-longhand-properties': null,
-        'declaration-block-no-shorthand-property-overrides': [
+        'declaration-block-no-redundant-longhand-properties': [
             true,
             {
-                severity: 'error',
+                ignoreShorthands: ['inset', 'overflow', 'margin-inline', 'inset-block'],
             },
         ],
+        'declaration-block-no-shorthand-property-overrides': true,
         'declaration-empty-line-before': [
             'always',
             {
                 except: ['first-nested', 'after-declaration'],
                 ignore: ['after-comment'],
-                severity: 'error',
             },
         ],
         'declaration-no-important': null,
         'declaration-property-value-disallowed-list': null,
         'font-family-name-quotes': null,
-        'font-family-no-duplicate-names': [
+        'font-family-no-duplicate-names': true,
+        'font-family-no-missing-generic-family-keyword': true,
+        'font-weight-notation': 'named-where-possible',
+        'function-calc-no-unspaced-operator': true,
+        'function-linear-gradient-no-nonstandard-direction': true,
+        'function-name-case': 'lower',
+        'function-no-unknown': [
             true,
             {
-                severity: 'error',
+                ignoreFunctions: ['fade'],
             },
         ],
-        'font-family-no-missing-generic-family-keyword': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
-        'function-calc-no-unspaced-operator': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
-        'function-linear-gradient-no-nonstandard-direction': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
-        'function-name-case': [
-            'lower',
-            {
-                severity: 'error',
-            },
-        ],
-        'function-no-unknown': null,
-        'function-url-no-scheme-relative': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
-        'function-url-quotes': null,
-        'keyframe-block-no-duplicate-selectors': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
-        'keyframe-declaration-no-important': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
+        'function-url-no-scheme-relative': true,
+        'function-url-quotes': 'always',
+        'keyframe-block-no-duplicate-selectors': true,
+        'keyframe-declaration-no-important': true,
         'length-zero-no-unit': [
             true,
             {
                 ignore: ['custom-properties'],
-                severity: 'error',
             },
         ],
-        'media-feature-name-no-unknown': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
+        'lightness-notation': 'percentage',
+        'media-feature-name-no-unknown': true,
         'media-feature-name-no-vendor-prefix': null,
-        'named-grid-areas-no-invalid': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
+        'media-feature-name-value-no-unknown': true,
+        'named-grid-areas-no-invalid': true,
         'no-descending-specificity': null,
-        'no-duplicate-at-import-rules': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
+        'no-duplicate-at-import-rules': true,
         'no-duplicate-selectors': true,
-        'no-empty-source': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
+        'no-empty-source': true,
+        'no-invalid-double-slash-comments': true,
         'no-invalid-position-at-import-rule': null,
-        'no-irregular-whitespace': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
+        'no-irregular-whitespace': true,
         'no-unknown-animations': null,
+        'no-unknown-custom-media': true,
         'order/properties-order': [
             [
                 'all',
@@ -249,12 +142,7 @@ module.exports = {
             },
         ],
         'property-disallowed-list': ['border-inline', 'padding-inline', 'margin-inline'],
-        'property-no-unknown': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
+        'property-no-unknown': true,
         'property-no-vendor-prefix': null,
         'rem-over-px/rem-over-px': [
             true,
@@ -292,69 +180,32 @@ module.exports = {
             {
                 except: ['first-nested'],
                 ignore: ['after-comment'],
-                severity: 'error',
             },
         ],
-        'selector-attribute-quotes': [
-            'always',
-            {
-                severity: 'error',
-            },
-        ],
+        'selector-anb-no-unmatchable': true,
+        'selector-attribute-quotes': 'always',
         'selector-max-id': null,
         'selector-max-specificity': null,
         'selector-max-type': null,
         'selector-no-qualifying-type': null,
-        'selector-no-vendor-prefix': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
-        'selector-pseudo-class-no-unknown': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
-        'selector-pseudo-element-colon-notation': [
-            'double',
-            {
-                severity: 'error',
-            },
-        ],
+        'selector-no-vendor-prefix': true,
+        'selector-pseudo-class-no-unknown': true,
+        'selector-pseudo-element-colon-notation': 'double',
         'selector-pseudo-element-no-unknown': [
             true,
             {
                 ignorePseudoElements: ['ng-deep'],
             },
         ],
-        'selector-type-case': [
-            'lower',
-            {
-                severity: 'error',
-            },
-        ],
+        'selector-type-case': 'lower',
         'selector-type-no-unknown': [
             true,
             {
-                ignore: ['custom-elements', 'default-namespace'],
-                ignoreNamespaces: ['/^my-/', /app/],
-                ignoreTypes: ['/^/deep/'],
+                ignoreTypes: ['/^tui-/'],
             },
         ],
-        'shorthand-property-no-redundant-values': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
-        'string-no-newline': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
+        'shorthand-property-no-redundant-values': true,
+        'string-no-newline': true,
         'time-min-milliseconds': null,
         'unit-allowed-list': [
             'px',
@@ -371,25 +222,14 @@ module.exports = {
             'vw',
             'fr',
         ],
-        'unit-no-unknown': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
+        'unit-no-unknown': true,
         'value-keyword-case': [
             'lower',
             {
                 ignoreKeywords: ['currentColor', 'backgroundColor', 'optimizeLegibility'],
                 ignoreProperties: ['/^--/', String.raw`/^\$/`],
-                severity: 'error',
             },
         ],
-        'value-no-vendor-prefix': [
-            true,
-            {
-                severity: 'error',
-            },
-        ],
+        'value-no-vendor-prefix': true,
     },
 };
